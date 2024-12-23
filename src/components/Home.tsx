7cb9847ae8bd49317c807752daa5359fcb6e8f19
pr4th4meshh <prathameshasolkar17@gmail.com>
2024-12-23 20:35:16 +0530

    add: quotes component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,6 +4,7 @@ import Footer from "./Footer"
 import Header from "./Header"
 import Hero from "./Hero"
 import ImagesCarousel from "./ImagesCarousel"
+import QuotesComponent from "./Quotes"
 
 const LandingPage = () => {
   return (
@@ -11,6 +12,7 @@ const LandingPage = () => {
       <Header />
       <Hero />
       <ImagesCarousel />
+      <QuotesComponent />
       <Footer />
     </div>
   )
