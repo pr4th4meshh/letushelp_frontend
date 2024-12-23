10b0ee4c96abf6613b2a821dfdb15d37e922d32e
pr4th4meshh <prathameshasolkar17@gmail.com>
2024-12-23 13:17:40 +0530

    fix: buttons ui on hover

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -44,15 +44,17 @@ const Hero = () => {
 
       {/* Action buttons */}
       <div className="flex flex-row justify-center z-50 gap-[21px] pt-[32px]">
-        <SecondaryButton
-          title="Find Opportunities"
-          className="text-[14px] font-[600] leading-[100%]"
-        />
-        <SecondaryButton
-          title="List Opportunities"
-          className="text-[14px] font-[600] leading-[100%] bg-[#0984E3] hover:bg-[#0984E3] text-white"
-        />
-      </div>
+      <SecondaryButton
+    title="Find Opportunities"
+    className="text-[14px] font-[600] leading-[100%] hero-button-shadow bg-[#0984E3] hover:bg-[#2169a0] text-white transition-all duration-300 transform hover:translate-y-[-4px]"
+  />
+  <SecondaryButton
+    title="List Opportunities"
+    className="text-[14px] font-[600] leading-[100%] hero-button-shadow transition-all duration-300 transform hover:translate-y-[-4px] hover:bg-[#2169a0] hover:text-white"
+  />
+
+</div>
+
     </div>
   )
 }
